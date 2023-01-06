28d4b469d09e98cedfb5e6398d12099bc475c8dd
Mack Rusing <mackrusing@gmail.com>
2023-01-06 12:32:45 -0700

    start random winner logic

--- frontend/static/resources/scripts/students.js
+++ frontend/static/resources/scripts/students.js
@@ -1,45 +1,43 @@
-// dom elements
-const studentsTableDiv = document.querySelector("#students-table-div");
-
 // update table info
+const studentsTableDiv = document.querySelector('#students-table-div');
 async function createStudentsTable() {
     // create requests to api
-    const urlBase = "/api/students?";
+    const urlBase = '/api/students?';
     const url9 = urlBase + new URLSearchParams({ gradeLvl: 9 });
     const url10 = urlBase + new URLSearchParams({ gradeLvl: 10 });
     const url11 = urlBase + new URLSearchParams({ gradeLvl: 11 });
     const url12 = urlBase + new URLSearchParams({ gradeLvl: 12 });
 
     const response9 = await fetch(url9, {
-        method: "GET",
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
     });
     const response10 = await fetch(url10, {
-        method: "GET",
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
     });
     const response11 = await fetch(url11, {
-        method: "GET",
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
     });
     const response12 = await fetch(url12, {
-        method: "GET",
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
     });
 
     // check for error
     if (!response9.ok || !response10.ok || !response11.ok || !response12.ok) {
         // throw error
-        location.href = "500.html";
-        throw new Error("problem w/ get req");
+        location.href = '500.html';
+        throw new Error('problem w/ get req');
     }
 
     // get students from response
@@ -60,80 +58,80 @@ async function createStudentsTable() {
         return b.points - a.points;
     });
 
-    const table = document.createElement("table");
-    const headerRow = document.createElement("tr");
+    const table = document.createElement('table');
+    const headerRow = document.createElement('tr');
 
-    const hc1 = document.createElement("th");
-    hc1.innerText = "First Name";
-    const hc2 = document.createElement("th");
-    hc2.innerText = "Last Name";
-    const hc3 = document.createElement("th");
-    hc3.innerText = "Points";
-    const hc4 = document.createElement("th");
-    hc4.innerText = "Grade";
+    const hc1 = document.createElement('th');
+    hc1.innerText = 'First Name';
+    const hc2 = document.createElement('th');
+    hc2.innerText = 'Last Name';
+    const hc3 = document.createElement('th');
+    hc3.innerText = 'Points';
+    const hc4 = document.createElement('th');
+    hc4.innerText = 'Grade';
 
     headerRow.append(hc1, hc2, hc3, hc4);
     table.append(headerRow);
 
     grade9.forEach((studentObj) => {
-        const row = document.createElement("tr");
-        row.setAttribute("class", "grade-9");
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'grade-9');
 
-        const c1 = document.createElement("td");
+        const c1 = document.createElement('td');
         c1.innerText = studentObj.firstName;
-        const c2 = document.createElement("td");
+        const c2 = document.createElement('td');
         c2.innerText = studentObj.lastName;
-        const c3 = document.createElement("td");
+        const c3 = document.createElement('td');
         c3.innerText = studentObj.points;
-        const c4 = document.createElement("td");
+        const c4 = document.createElement('td');
         c4.innerText = studentObj.gradeLvl;
 
         row.append(c1, c2, c3, c4);
         table.append(row);
     });
     grade10.forEach((studentObj) => {
-        const row = document.createElement("tr");
-        row.setAttribute("class", "grade-10");
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'grade-10');
 
-        const c1 = document.createElement("td");
+        const c1 = document.createElement('td');
         c1.innerText = studentObj.firstName;
-        const c2 = document.createElement("td");
+        const c2 = document.createElement('td');
         c2.innerText = studentObj.lastName;
-        const c3 = document.createElement("td");
+        const c3 = document.createElement('td');
         c3.innerText = studentObj.points;
-        const c4 = document.createElement("td");
+        const c4 = document.createElement('td');
         c4.innerText = studentObj.gradeLvl;
 
         row.append(c1, c2, c3, c4);
         table.append(row);
     });
     grade11.forEach((studentObj) => {
-        const row = document.createElement("tr");
-        row.setAttribute("class", "grade-11");
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'grade-11');
 
-        const c1 = document.createElement("td");
+        const c1 = document.createElement('td');
         c1.innerText = studentObj.firstName;
-        const c2 = document.createElement("td");
+        const c2 = document.createElement('td');
         c2.innerText = studentObj.lastName;
-        const c3 = document.createElement("td");
+        const c3 = document.createElement('td');
         c3.innerText = studentObj.points;
-        const c4 = document.createElement("td");
+        const c4 = document.createElement('td');
         c4.innerText = studentObj.gradeLvl;
 
         row.append(c1, c2, c3, c4);
         table.append(row);
     });
     grade12.forEach((studentObj) => {
-        const row = document.createElement("tr");
-        row.setAttribute("class", "grade-12");
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'grade-12');
 
-        const c1 = document.createElement("td");
+        const c1 = document.createElement('td');
         c1.innerText = studentObj.firstName;
-        const c2 = document.createElement("td");
+        const c2 = document.createElement('td');
         c2.innerText = studentObj.lastName;
-        const c3 = document.createElement("td");
+        const c3 = document.createElement('td');
         c3.innerText = studentObj.points;
-        const c4 = document.createElement("td");
+        const c4 = document.createElement('td');
         c4.innerText = studentObj.gradeLvl;
 
         row.append(c1, c2, c3, c4);
@@ -142,6 +140,24 @@ async function createStudentsTable() {
 
     // update element
     studentsTableDiv.replaceChildren(table);
+}
+
+// pick winner
+const pickWinnerButton = document.querySelector('.pick-winners');
+async function pickWinner() {
+    // get random winners
+    const grade9Rows = studentsTableDiv.querySelectorAll('table .grade-9');
+    const grade10Rows = studentsTableDiv.querySelectorAll('table .grade-10');
+    const grade11Rows = studentsTableDiv.querySelectorAll('table .grade-11');
+    const grade12Rows = studentsTableDiv.querySelectorAll('table .grade-12');
+    const grade9Winner =
+        grade9Rows[Math.floor(Math.random() * grade9Rows.length)];
+    const grade10Winner =
+        grade10Rows[Math.floor(Math.random() * grade10Rows.length)];
+    const grade11Winner =
+        grade11Rows[Math.floor(Math.random() * grade11Rows.length)];
+    const grade12Winner =
+        grade12Rows[Math.floor(Math.random() * grade12Rows.length)];
 }
 
 // run function
